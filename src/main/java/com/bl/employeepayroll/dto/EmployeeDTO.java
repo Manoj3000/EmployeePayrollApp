8ed3d33011c6af3ef2dd881329861ad36e7c7830
manoj gaikwad <gaikwadm853@gmail.com>
2021-11-22 01:03:48 +0530

    Used Lombok to auto generate getters and setters

--- src/main/java/com/bl/employeepayroll/dto/EmployeeDTO.java
+++ src/main/java/com/bl/employeepayroll/dto/EmployeeDTO.java
@@ -1,39 +1,16 @@
 package com.bl.employeepayroll.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmployeeDTO {
 
 	private int id;
 	private String name;
 	private double salary;
-	
-	public EmployeeDTO(int id, String name, double salary) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-	}
-	
-	public EmployeeDTO() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public double getSalary() {
-		return salary;
-	}
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
+
 }
--- src/main/java/com/bl/employeepayroll/model/Employee.java
+++ src/main/java/com/bl/employeepayroll/model/Employee.java
@@ -9,7 +9,12 @@ import javax.persistence.Id;
 
 import com.bl.employeepayroll.dto.EmployeeDTO;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@NoArgsConstructor
 public class Employee {
 
 	@Id
@@ -19,76 +24,12 @@ public class Employee {
 	private double salary;
 	private LocalDateTime created_at;
 	private LocalDateTime updated_at;
-	
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public double getSalary() {
-		return salary;
-	}
-
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-
-	public LocalDateTime getCreated_at() {
-		return created_at;
-	}
-
-	public void setCreated_at(LocalDateTime created_at) {
-		this.created_at = created_at;
-	}
-
-	public LocalDateTime getUpdated_at() {
-		return updated_at;
-	}
-
-	public void setUpdated_at(LocalDateTime updated_at) {
-		this.updated_at = updated_at;
-	}
-
-	public Employee(long id, String name, double salary) {
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-		this.created_at = LocalDateTime.now();
-	}
 
 	public Employee(EmployeeDTO employeeDTO) {	
 		this.id = employeeDTO.getId();
 		this.name = employeeDTO.getName();
 		this.salary = employeeDTO.getSalary();
 		this.created_at = LocalDateTime.now();
-	}
-
-	public Employee() {	}
-
-	public Employee(long id, String name, double salary, LocalDateTime created_at, LocalDateTime updated_at) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-		this.created_at = created_at;
-		this.updated_at = updated_at;
-	}
-
-	@Override
-	public String toString() {
-		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", created_at=" + created_at
-				+ ", updated_at=" + updated_at + "]";
 	}
 
 }
