f86f888419207d4c4197e653d5cb96a739f6bfc7
manoj gaikwad <gaikwadm853@gmail.com>
2021-11-22 20:17:11 +0530

    User Logger for print welcome message

--- src/main/java/com/bl/employeepayroll/EmployeePayrollAppApplication.java
+++ src/main/java/com/bl/employeepayroll/EmployeePayrollAppApplication.java
@@ -1,13 +1,17 @@
 package com.bl.employeepayroll;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmployeePayrollAppApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(EmployeePayrollAppApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollAppApplication.class, args);
+		logger.info("Welcome To Employee Payroll AppApplication");
 	}
 }
 
